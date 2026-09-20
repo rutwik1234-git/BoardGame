fdf10ab81a2f3a82b5baeff0426e5fefd9fcedf6
root <root@ip-172-31-6-51.ap-south-1.compute.internal>
2026-09-20 09:20:55 +0000

    new commit

--- src/main/java/com/javaproject/controllers/BoardGameController.java
+++ src/main/java/com/javaproject/controllers/BoardGameController.java
@@ -3,14 +3,9 @@ package com.javaproject.controllers;
 import java.net.URI;
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
-import org.hibernate.boot.model.relational.Database;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +18,6 @@ import com.javaproject.beans.ErrorMessage;
 import com.javaproject.beans.Review;
 import com.javaproject.database.DatabaseAccess;
 
-// special type of controller that is specialized for REST purpose. It marshals our domain objects to and from json
 @RestController
 @RequestMapping("/boardgames")
 public class BoardGameController {
@@ -36,8 +30,6 @@ public class BoardGameController {
 
     /**
      * Retrieve all boardgames
-     * 
-     * @return
      */
     @GetMapping
     public List<BoardGame> getBoardGames() {
@@ -45,31 +37,16 @@ public class BoardGameController {
     }
 
     /**
-     * Handles requests for specific boardgame
-     * 
-     * @param id
-     * @return the ResponseEntity
+     * Retrieve a specific boardgame by ID
      */
     @GetMapping("/{id}")
     public ResponseEntity<?> getBoardGame(@PathVariable Long id) {
-        BoardGame boardGame = da.getBoardGame(id);
-        if (boardGame != null) {
-            return ResponseEntity.ok(boardGame);
+        BoardGame game = da.getBoardGameById(id);
+        if (game != null) {
+            return ResponseEntity.ok(game);
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessage("No such record"));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorMessage("Board game with ID " + id + " not found."));
         }
-    }
-
-    @PostMapping(consumes = "application/json")
-    public ResponseEntity<?> postBoardGame(@RequestBody BoardGame boardGame) {
-        try {
-            Long id = da.addBoardGame(boardGame);
-            boardGame.setId(id);
-            URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
-            return ResponseEntity.created(location).body(boardGame);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorMessage("Name already exists."));
-        }
-
     }
 }
