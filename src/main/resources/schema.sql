7dbc6fd955372c4d8268e78bdd03742f3a9b0246
rutwik1234-git <pawarrutwik262001@gmail.com>
2026-09-20 16:08:29 +0530

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
--- 1. Spring Security default schema
+-- Spring Security Default Schema
 CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) NOT NULL PRIMARY KEY,
     password VARCHAR(500) NOT NULL,
@@ -8,24 +8,24 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS authorities (
     username VARCHAR(50) NOT NULL,
     authority VARCHAR(50) NOT NULL,
-    CONSTRAINT fk_authorities_users FOREIGN KEY(username) REFERENCES users(username)
+    CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES users(username)
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS ix_auth_username ON authorities (username, authority);
 
--- 2. Domain application tables
+-- Boardgame Application Schema
 CREATE TABLE IF NOT EXISTS boardgames (
     id          BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name        VARCHAR(128) NOT NULL,
-    level       INT NOT NULL,
-    minPlayers  INT NOT NULL,
-    maxPlayers  VARCHAR(50) NOT NULL, -- Set to VARCHAR to allow '+' values
-    gameType    VARCHAR(50) NOT NULL
+    level       INT NOT NULL DEFAULT 1,
+    min_players INT NOT NULL,
+    max_players VARCHAR(50) NOT NULL,
+    game_type   VARCHAR(50) NOT NULL DEFAULT 'General'
 );
 
 CREATE TABLE IF NOT EXISTS reviews (
     id      BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    gameId  BIGINT NOT NULL,
+    game_id BIGINT NOT NULL,
     `text`  VARCHAR(1024) NOT NULL UNIQUE,
-    CONSTRAINT game_review_fk FOREIGN KEY (gameId) REFERENCES boardgames (id) ON DELETE CASCADE
+    CONSTRAINT game_review_fk FOREIGN KEY (game_id) REFERENCES boardgames (id) ON DELETE CASCADE
 );
